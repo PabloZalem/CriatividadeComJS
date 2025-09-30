7c89ca7a8e085ba96228c0eac57ddb433939eeb3
Pablo Zalem <zalem.pablo@yahoo.com>
2025-09-30 19:28:01 -0300

    Metodo Strings

--- Textos.js
+++ Textos.js
@@ -30,3 +30,19 @@ msg = "Hello, " + "world" + "!"; // concatenação com o operador +
 greeting = "Welcome to " + msg;
 console.log(greeting); // => Welcome to Hello, world!
 console.log(greeting.length); // => 26
+
+// Metodos do String
+var s = "hello world";
+console.log(s.charAt(0)); // => "h"
+console.log(s.charAt(9)); // => "l"
+console.log(s.charAt(s.length-1)); // => "d"
+console.log(s.slice(1,4)); // => "ell"
+console.log(s.slice(-2)); // => "ld"
+console.log(s.indexOf("l")); // => 2
+console.log(s.lastIndexOf("l")); // => 9
+console.log(s.indexOf("l", 3)); // => 3
+console.log(s.split(", ")); // => ["hello", "world"]
+console.log(s.replace("h", "H")); // => "Hello world"
+console.log(s.toUpperCase()); // => "HELLO WORLD"
+console.log(s.toLowerCase()); // => "hello world"
+console.log(s.trim()); // => "hello world" (sem espaços em branco no início ou fim)
